99157f855996b2776e5b2fc517092cd631b19421
Joe Huss <detain@interserver.net>
2026-06-29 08:46:06 -0400

    B3/F3: add LibraryRef DTO and strict mode for HeartbeatDto.libraries

--- tests/Hub/HeartbeatDtoTest.php
+++ tests/Hub/HeartbeatDtoTest.php
@@ -36,6 +36,117 @@ final class HeartbeatDtoTest extends TestCase
         $this->assertSame(self::full(), $dto->toPayload());
     }
 
+    public function test_library_round_trip(): void
+    {
+        $payload = self::full();
+        $dto = HeartbeatDto::fromPayload($payload);
+
+        $output = $dto->toPayload();
+        $this->assertIsArray($output['libraries']);
+        $this->assertCount(1, $output['libraries']);
+        $this->assertSame('lib-1', $output['libraries'][0]['library_id']);
+        $this->assertSame('Movies', $output['libraries'][0]['library_name']);
+    }
+
+    public function test_strict_libraries_throws_on_missing_library_id(): void
+    {
+        $payload = self::full();
+        $payload['libraries'] = [['library_name' => 'Movies']];
+
+        $this->expectException(InvalidArgumentException::class);
+        HeartbeatDto::fromPayload($payload, strictLibraries: true);
+    }
+
+    public function test_strict_libraries_throws_on_empty_library_id(): void
+    {
+        $payload = self::full();
+        $payload['libraries'] = [['library_id' => '', 'library_name' => 'Movies']];
+
+        $this->expectException(InvalidArgumentException::class);
+        HeartbeatDto::fromPayload($payload, strictLibraries: true);
+    }
+
+    public function test_strict_libraries_throws_on_missing_library_name(): void
+    {
+        $payload = self::full();
+        $payload['libraries'] = [['library_id' => 'lib-1']];
+
+        $this->expectException(InvalidArgumentException::class);
+        HeartbeatDto::fromPayload($payload, strictLibraries: true);
+    }
+
+    public function test_strict_libraries_throws_on_empty_library_name(): void
+    {
+        $payload = self::full();
+        $payload['libraries'] = [['library_id' => 'lib-1', 'library_name' => '']];
+
+        $this->expectException(InvalidArgumentException::class);
+        HeartbeatDto::fromPayload($payload, strictLibraries: true);
+    }
+
+    public function test_lenient_libraries_skips_missing_library_id(): void
+    {
+        $payload = self::full();
+        $payload['libraries'] = [
+            ['library_id' => 'lib-1', 'library_name' => 'Movies'],
+            ['library_name' => 'TV Shows'],
+        ];
+
+        $dto = HeartbeatDto::fromPayload($payload, strictLibraries: false);
+        $output = $dto->toPayload();
+
+        /** @var list<array{library_id: string, library_name: string}> $libraries */
+        $libraries = $output['libraries'];
+        $this->assertCount(1, $libraries);
+        $this->assertSame('lib-1', $libraries[0]['library_id']);
+    }
+
+    public function test_lenient_libraries_skips_missing_library_name(): void
+    {
+        $payload = self::full();
+        $payload['libraries'] = [
+            ['library_id' => 'lib-1', 'library_name' => 'Movies'],
+            ['library_id' => 'lib-2'],
+        ];
+
+        $dto = HeartbeatDto::fromPayload($payload, strictLibraries: false);
+        $output = $dto->toPayload();
+
+        /** @var list<array{library_id: string, library_name: string}> $libraries */
+        $libraries = $output['libraries'];
+        $this->assertCount(1, $libraries);
+        $this->assertSame('lib-1', $libraries[0]['library_id']);
+    }
+
+    public function test_lenient_libraries_skips_non_array_entry(): void
+    {
+        $payload = self::full();
+        $payload['libraries'] = [
+            ['library_id' => 'lib-1', 'library_name' => 'Movies'],
+            'not-an-array',
+        ];
+
+        $dto = HeartbeatDto::fromPayload($payload, strictLibraries: false);
+        $output = $dto->toPayload();
+
+        /** @var list<array{library_id: string, library_name: string}> $libraries */
+        $libraries = $output['libraries'];
+        $this->assertCount(1, $libraries);
+        $this->assertSame('lib-1', $libraries[0]['library_id']);
+    }
+
+    public function test_strict_libraries_throws_on_non_array_entry(): void
+    {
+        $payload = self::full();
+        $payload['libraries'] = [
+            ['library_id' => 'lib-1', 'library_name' => 'Movies'],
+            'not-an-array',
+        ];
+
+        $this->expectException(InvalidArgumentException::class);
+        HeartbeatDto::fromPayload($payload, strictLibraries: true);
+    }
+
     public function test_missing_serverId_throws(): void
     {
         $payload = self::full();
